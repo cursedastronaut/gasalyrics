f3ff679ff054cdf892422657b28057f16edde9ff
Martineaux Galaad <galaad.martineaux@universite-paris-saclay.fr>
2024-05-04 22:16:26 +0200

    Reworked navbar to have a page to an album instead of listing them all in it. Closed #3

--- index.php
+++ index.php
@@ -9,36 +9,41 @@
 		<link rel="stylesheet" href="./res/css/footer.css"		type="text/css">
 	</head>
 	<body>
-		<?php
-		include_once("res/php/navbar.php");
-		$gs = new GasaLyricsDB();
+		<div class="flex">
+			<div>
+				<?php
+				include_once("res/php/navbar.php");
+				$gs = new GasaLyricsDB();
 
-		if (!is_null($_GET["song_id"]))
-		{
-			include_once("res/php/lyrics.php");
-		}
-		else if (!is_null($_GET["album_id"]))
-		{
-			$albumTitle = $gs->getAlbumTitle($_GET["album_id"], false);
-			if ($albumTitle != -1) {
-				echo "<title>" . $albumTitle . " - GasaLyrics</title>";
-				include_once("res/php/song_list.php");
-			}
-			else {
-				$gs->Error404("Album");
-			}
-		}
-		else {
-			$result = $gs->getAlbums();
-			while ($row =  mysql_fetch_assoc($result)) {
-				echo "<a href='index.php?album_id=" . $row["idAlbum"] . "' class='album_block';>"
-				. "<center><div class='ablum_icon' style='background:url(" . $row["iconLinkAlbum"]. ");background-size:contain;'></div></center>"
-				. $row["titleAlbum"] . "</a>";
-			}
-			echo "<title>GasaLyrics</title>";
-		}
-		include("res/php/footer.php");
-		?>
+				if (!is_null($_GET["song_id"]))
+				{
+					include_once("res/php/lyrics.php");
+				}
+				else if (!is_null($_GET["album_id"]))
+				{
+					$albumTitle = $gs->getAlbumTitle($_GET["album_id"], false);
+					if ($albumTitle != -1) {
+						echo "<title>" . $albumTitle . " - GasaLyrics</title>";
+						include_once("res/php/song_list.php");
+					}
+					else {
+						$gs->Error404("Album");
+					}
+				}
+				else {
+					$result = $gs->getAlbums();
+					while ($row =  mysql_fetch_assoc($result)) {
+						echo "<a href='index.php?album_id=" . $row["idAlbum"] . "' class='album_block';>"
+						. "<center><div class='ablum_icon' style='background:url(" . $row["iconLinkAlbum"]. ");background-size:contain;'></div></center>"
+						. $row["titleAlbum"] . "</a>";
+					}
+					echo "<title>GasaLyrics</title>";
+				}
+				?>
+			</div>
+			<?php include("res/php/albums.php"); ?>
+		</div>
+		<?php include("res/php/footer.php"); ?>
 	</body>
 </html>
 
