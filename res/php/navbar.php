256180c0a6904aa94df0776f01a57b88bc72ac4c
cursedastronaut <g.martineaux@student.isartdigital.com>
2024-05-04 18:41:05 +0200

    Fixed navbar causing FATAL when there is no Album.

--- res/php/navbar.php
+++ res/php/navbar.php
@@ -8,10 +8,12 @@
 	$gs_nav = new GasaLyricsDB();
 	$albums = $gs_nav->getAlbums();
 
+	if ($albums != -1) {
 	while ($row = mysql_fetch_assoc($albums)) {
 		echo	"<a href='index.php?album_id=" . $row["idAlbum"] . "'>"
 		.		"" . $row["shortTitle"]
 		.		"</a>";
+	}
 	}
 	?>
 </header>
