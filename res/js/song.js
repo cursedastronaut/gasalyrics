bb198429abe9a3293bce6e239dcfe561fb1de592
cursedastronaut <g.martineaux@student.isartdigital.com>
2024-05-04 15:21:38 +0200

    Changed look of language buttons on songs lyrics page

--- res/js/song.js
+++ res/js/song.js
@@ -2,7 +2,9 @@ function toggleDisplay(id) {
 	const element = document.getElementById(id);
 	if (element.style.fontSize === "0px" || element.style.fontSize === '') {
 		element.style.fontSize = "inherit";
+		element.parentElement.style.paddingInline = "var(--lyrics-padding-inline)";
 	} else {
 		element.style.fontSize = "0px";
+		element.parentElement.style.paddingInline = "0px";
 	}
 }
