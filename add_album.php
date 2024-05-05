e4979c99693b8d583ea130600a146b533cce42a4
cursedastronaut <g.martineaux@student.isartdigital.com>
2024-05-05 13:40:57 +0200

    Fixed both footer and sidebar not appearing on add, managing, and add album pages. Closed #9 Closed #6

--- add_album.php
+++ add_album.php
@@ -7,41 +7,45 @@
 		<link rel="stylesheet" href="./res/css/song_list.css"	type="text/css">
 		<link rel="stylesheet" href="./res/css/song.css"		type="text/css">
 		<link rel="stylesheet" href="./res/css/managing.css"	type="text/css">
+		<link rel="stylesheet" href="./res/css/footer.css"		type="text/css">
 	</head>
 	<body>
-		<?php include_once("res/php/navbar.php") ?>
-		<h1>Add an album</h1>
-		
-		<?php
-		include_once("res/php/connection.php");
-		$gs = new GasaLyricsDB();
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$gs->askSQL(
-				"INSERT INTO Album (titleAlbum, iconLinkAlbum, shortTitle) VALUES ('" 
-				. $gs->sanitize($_POST["titleAlbum"]) 
-				. "','" . $gs->sanitize($_POST["iconLink"]) . "','"
-				. $gs->sanitize($_POST["titleShort"]) . "');"
-				, $_POST["username"], $_POST["password"]
-			);
-		}
-		?>
-		<h1>Add an album</h1>
+		<div class="flex">
+			<div style="width: 100%;">
+				<?php include_once("res/php/navbar.php") ?>
+				<?php
+				include_once("res/php/connection.php");
+				$gs = new GasaLyricsDB();
+				if ($_SERVER["REQUEST_METHOD"] == "POST") {
+					$gs->askSQL(
+						"INSERT INTO Album (titleAlbum, iconLinkAlbum, shortTitle) VALUES ('" 
+						. $gs->sanitize($_POST["titleAlbum"]) 
+						. "','" . $gs->sanitize($_POST["iconLink"]) . "','"
+						. $gs->sanitize($_POST["titleShort"]) . "');"
+						, $_POST["username"], $_POST["password"]
+					);
+				}
+				?>
+				<h1>Add an album</h1>
 
-		<form id="songToAdd" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
-			<button type="submit">Submit</button>
-			<label for="username">Username</label>
-			<input type="text" name="username"><br>
-			<label for="password">Password</label>
-			<input type="password" name="password"><br>
-			<br>
-			<label for="titleAlbum">Full album title</label>
-			<input type="text" name="titleAlbum"><br>
-			<label for="titleShort">Short album title</label>
-			<input type="text" name="titleShort"><br>
-			<label for="iconLink">Link to the Album Icon (res/jpg/album_list/...)</label>
-			<input type="text" name="iconLink"><br>
-		</form>
-
+				<form id="songToAdd" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
+					<button type="submit">Submit</button>
+					<label for="username">Username</label>
+					<input type="text" name="username"><br>
+					<label for="password">Password</label>
+					<input type="password" name="password"><br>
+					<br>
+					<label for="titleAlbum">Full album title</label>
+					<input type="text" name="titleAlbum"><br>
+					<label for="titleShort">Short album title</label>
+					<input type="text" name="titleShort"><br>
+					<label for="iconLink">Link to the Album Icon (res/jpg/album_list/...)</label>
+					<input type="text" name="iconLink"><br>
+				</form>
+			</div>
+			<?php include("res/php/albums.php"); ?>
+		</div>
+		<?php include("res/php/footer.php"); ?>
 	</body>
 </html>
 
--- manage.php
+++ manage.php
@@ -7,21 +7,27 @@
 		<link rel="stylesheet" href="./res/css/song_list.css"	type="text/css">
 		<link rel="stylesheet" href="./res/css/song.css"		type="text/css">
 		<link rel="stylesheet" href="./res/css/managing.css"	type="text/css">
+		<link rel="stylesheet" href="./res/css/footer.css"		type="text/css">
 	</head>
 	<body>
-		<?php include_once("res/php/navbar.php") ?>
+		<div class="flex">
+			<div style="width: 100%;">
+				<?php include_once("res/php/navbar.php") ?>
 
-		
-		<?php
-			if (!is_null($_GET["song_id"])) {
-				include_once("res/php/edit_song.php");
-			} else if (!is_null($_GET["album_id"])) {
-				include_once("res/php/select_song.php");
-			} else {
-				include_once("res/php/select_album.php");
-			}
-		?>
-
+				
+				<?php
+					if (!is_null($_GET["song_id"])) {
+						include_once("res/php/edit_song.php");
+					} else if (!is_null($_GET["album_id"])) {
+						include_once("res/php/select_song.php");
+					} else {
+						include_once("res/php/select_album.php");
+					}
+				?>
+			</div>
+			<?php include("res/php/albums.php"); ?>
+		</div>
+		<?php include("res/php/footer.php"); ?>
 	</body>
 </html>
 
