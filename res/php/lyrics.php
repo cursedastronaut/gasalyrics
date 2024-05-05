18f4bb533e9e00db4a3376430c34ccd82f633ff0
cursedastronaut <g.martineaux@student.isartdigital.com>
2024-05-05 13:44:46 +0200

    Fixed both footer and sidebar not appearing on 404 page. Closed #10 Closed #7

--- res/php/lyrics.php
+++ res/php/lyrics.php
@@ -6,42 +6,40 @@
 	$result = $gs->getSongs(-1, $_GET["song_id"]);
 	if ($result == -1) {
 		$gs->Error404("Song");
-		die("");
-	}
-
+	} else {
+		$row = mysql_fetch_assoc($result);
+		$album = $gs->getAlbumById($row["albumIdSongs"]);
+		
+		if ($row["titleOriginalSongs"] == "" || $row["titleOriginalSongs"] == NULL)
+			$toshow = $row["titleSongs"];
+		else
+			$toshow = $row["titleOriginalSongs"];
 	
-	$row = mysql_fetch_assoc($result);
-	$album = $gs->getAlbumById($row["albumIdSongs"]);
 	
-	if ($row["titleOriginalSongs"] == "" || $row["titleOriginalSongs"] == NULL)
-		$toshow = $row["titleSongs"];
-	else
-		$toshow = $row["titleOriginalSongs"];
-
-
-	$toshow = $gs->transformToEntities($toshow, false);
-	$titleSongEnglish = $gs->transformToEntities($row["titleSongs"], false);
-
-	echo ""
-	. "<!-- Head - Partie 'Meta' -->"
-	. "<meta property='og:type' content='website'>"
-	. "<meta property='og:title' content='" . $toshow . " - GasaLyrics'>"
-	. "<meta property='og:description' content='" . $toshow
-	. " (" . $titleSongEnglish . ") on GasaLyrics, in the album " . $album['titleAlbum']
-	. ".'>"
-	. "<meta property='og:image' content='" . $gs->getWebsiteURL() . $album['iconLinkAlbum'] . "'>"
-	. "<meta name='theme-color' content='#0094FF'>";
-
-	echo "<h1>" . $row["titleSongs"] . "</h1>";
-	if (!is_null($row["titleOriginalSongs"]))
-		echo "<h2>" . $row["titleOriginalSongs"] . "</h2>";
-
-	echo "<title>" . $row["titleSongs"] . " - GasaLyrics</title>";
-
-	$result = $gs->getLyrics($_GET["song_id"]);
-	for ($i = 0; $row =  mysql_fetch_assoc($result); $i+=1) {
-		echo "<center><lyrics>" . "<button class='language' onclick='toggleDisplay(`lyrics-p" . $i . "`)'>" . $gs->langToDisplayName($row["idLang"]) . "</button>";
-		echo "<p id='lyrics-p" . $i . "'>" . $row["contentLyrics"] . "</p></lyrics></center>";
+		$toshow = $gs->transformToEntities($toshow, false);
+		$titleSongEnglish = $gs->transformToEntities($row["titleSongs"], false);
+	
+		echo ""
+		. "<!-- Head - Partie 'Meta' -->"
+		. "<meta property='og:type' content='website'>"
+		. "<meta property='og:title' content='" . $toshow . " - GasaLyrics'>"
+		. "<meta property='og:description' content='" . $toshow
+		. " (" . $titleSongEnglish . ") on GasaLyrics, in the album " . $album['titleAlbum']
+		. ".'>"
+		. "<meta property='og:image' content='" . $gs->getWebsiteURL() . $album['iconLinkAlbum'] . "'>"
+		. "<meta name='theme-color' content='#0094FF'>";
+	
+		echo "<h1>" . $row["titleSongs"] . "</h1>";
+		if (!is_null($row["titleOriginalSongs"]))
+			echo "<h2>" . $row["titleOriginalSongs"] . "</h2>";
+	
+		echo "<title>" . $row["titleSongs"] . " - GasaLyrics</title>";
+	
+		$result = $gs->getLyrics($_GET["song_id"]);
+		for ($i = 0; $row =  mysql_fetch_assoc($result); $i+=1) {
+			echo "<center><lyrics>" . "<button class='language' onclick='toggleDisplay(`lyrics-p" . $i . "`)'>" . $gs->langToDisplayName($row["idLang"]) . "</button>";
+			echo "<p id='lyrics-p" . $i . "'>" . $row["contentLyrics"] . "</p></lyrics></center>";
+		}
 	}
 	?>
 
