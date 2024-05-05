374ec1e69f8c62d0d4e4a35c605fe55109e97e92
Martineaux Galaad <galaad.martineaux@universite-paris-saclay.fr>
2024-05-05 13:40:57 +0200

    Fixed both footer and sidebar not appearing on add, managing, and add album pages. Closed #9 Closed #6

--- add.php
+++ add.php
@@ -7,68 +7,74 @@
 		<link rel="stylesheet" href="./res/css/song_list.css"	type="text/css">
 		<link rel="stylesheet" href="./res/css/song.css"		type="text/css">
 		<link rel="stylesheet" href="./res/css/managing.css"	type="text/css">
+		<link rel="stylesheet" href="./res/css/footer.css"		type="text/css">
 	</head>
 	<body>
-		<?php include_once("res/php/navbar.php") ?>
-		<h1>Add a song</h1>
-		
-		<?php
-		include_once("res/php/connection.php");
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$gs = new GasaLyricsDB();
-			echo $_POST["titleText"] . "<br>" . $_POST["titleOrText"] ."<br>" . $_POST["albumText"] . "<br>" . $_POST["langCode"] . "<br>" . $_POST["inputText1"] . "<br>" . $_POST["inputText2"];
-			echo "<br><br>";
-			$sql = "INSERT INTO Songs (titleSongs, titleOriginalSongs, albumIdSongs) VALUES ('" . $gs->sanitize($_POST["titleText"]) . "','" . $gs->sanitize($_POST["titleOrText"]) . "'," . $gs->sanitize($_POST["albumText"]) . ")";
-			$result = $gs->askSQL($sql, $_POST["username"], $_POST["password"]);
-			$sql = "SELECT idSongs FROM Songs WHERE titleSongs='" . $gs->sanitize($_POST["titleText"]) . "' AND titleOriginalSongs='" . $gs->sanitize($_POST["titleOrText"]) . "' AND albumIdSongs=" . $gs->sanitize($_POST["albumText"]);
-			$result = $gs->askSQL($sql);
-			$row =  mysql_fetch_assoc($result);
-			$numSong = $row["idSongs"];
-			for ($numInputText = 1; !is_null($_POST["inputText" . ($numInputText)]); $numInputText++) 
-			{
-				$sql = "INSERT INTO Lyrics (idLang, contentLyrics, idSongsLyrics) VALUES ("
-				. intval(str_replace("lang_", "", $gs->sanitize($_POST["langCode" . $numInputText])))
-				. ", '" . $gs->sanitize($_POST["inputText" . $numInputText]) . "',  " . $numSong . ")";
-				$result = $gs->askSQL($sql, $_POST["username"], $_POST["password"]);
-				echo $sql . "<br>";
-			}
-		}
-		?>
-		<h1>Add lyrics</h1>
-		<form id="songToAdd" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
-			<button type="submit">Submit</button>
-			<label for="username">Username</label>
-			<input type="text" name="username"><br>
-			<label for="password">Password</label>
-			<input type="password" name="password"><br>
-			<br>
-			<label for="titleText">English Title</label>
-			<input type="text" name="titleText"><br>
-			<label for="titleOrText">Original title</label>
-			<input type="text" name="titleOrText" id="title0"><br>
-			<label for="albumText">Album number</label>
-			<input type="text" name="albumText"><br>
-			<hr>
-			<label for="langCode1">Language</label>
-			<select type="text" name="langCode1"><br>
-			<?php
-				$gs = new GasaLyricsDB();
-				$languages = $gs->getLanguages();
-				echo $languages;
-				if ($languages != -1) {
-					while ($row = mysql_fetch_assoc($languages)) {
-						echo	"<option value='lang_" . $row["idLang"] . "'>"
-						.		"" . $row["nameLang"]
-						.		"</option>";
+		<div class="flex">
+			<div style="width: 100%;">
+				<?php include_once("res/php/navbar.php") ?>
+				<h1>Add a song</h1>
+				
+				<?php
+				include_once("res/php/connection.php");
+				if ($_SERVER["REQUEST_METHOD"] == "POST") {
+					$gs = new GasaLyricsDB();
+					echo $_POST["titleText"] . "<br>" . $_POST["titleOrText"] ."<br>" . $_POST["albumText"] . "<br>" . $_POST["langCode"] . "<br>" . $_POST["inputText1"] . "<br>" . $_POST["inputText2"];
+					echo "<br><br>";
+					$sql = "INSERT INTO Songs (titleSongs, titleOriginalSongs, albumIdSongs) VALUES ('" . $gs->sanitize($_POST["titleText"]) . "','" . $gs->sanitize($_POST["titleOrText"]) . "'," . $gs->sanitize($_POST["albumText"]) . ")";
+					$result = $gs->askSQL($sql, $_POST["username"], $_POST["password"]);
+					$sql = "SELECT idSongs FROM Songs WHERE titleSongs='" . $gs->sanitize($_POST["titleText"]) . "' AND titleOriginalSongs='" . $gs->sanitize($_POST["titleOrText"]) . "' AND albumIdSongs=" . $gs->sanitize($_POST["albumText"]);
+					$result = $gs->askSQL($sql);
+					$row =  mysql_fetch_assoc($result);
+					$numSong = $row["idSongs"];
+					for ($numInputText = 1; !is_null($_POST["inputText" . ($numInputText)]); $numInputText++) 
+					{
+						$sql = "INSERT INTO Lyrics (idLang, contentLyrics, idSongsLyrics) VALUES ("
+						. intval(str_replace("lang_", "", $gs->sanitize($_POST["langCode" . $numInputText])))
+						. ", '" . $gs->sanitize($_POST["inputText" . $numInputText]) . "',  " . $numSong . ")";
+						$result = $gs->askSQL($sql, $_POST["username"], $_POST["password"]);
+						echo $sql . "<br>";
 					}
 				}
-			?>
-			</select>
-			<label for="inputText1">Lyrics</label>
-			<textarea type="text" name="inputText1" id="lyric1"></textarea><br>
-		</form>
-		<button class="add-language" onclick="createInput()">Add a language</button>
-
+				?>
+				<h1>Add lyrics</h1>
+				<form id="songToAdd" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
+					<button type="submit">Submit</button>
+					<label for="username">Username</label>
+					<input type="text" name="username"><br>
+					<label for="password">Password</label>
+					<input type="password" name="password"><br>
+					<br>
+					<label for="titleText">English Title</label>
+					<input type="text" name="titleText"><br>
+					<label for="titleOrText">Original title</label>
+					<input type="text" name="titleOrText" id="title0"><br>
+					<label for="albumText">Album number</label>
+					<input type="text" name="albumText"><br>
+					<hr>
+					<label for="langCode1">Language</label>
+					<select type="text" name="langCode1"><br>
+					<?php
+						$gs = new GasaLyricsDB();
+						$languages = $gs->getLanguages();
+						echo $languages;
+						if ($languages != -1) {
+							while ($row = mysql_fetch_assoc($languages)) {
+								echo	"<option value='lang_" . $row["idLang"] . "'>"
+								.		"" . $row["nameLang"]
+								.		"</option>";
+							}
+						}
+					?>
+					</select>
+					<label for="inputText1">Lyrics</label>
+					<textarea type="text" name="inputText1" id="lyric1"></textarea><br>
+				</form>
+				<button class="add-language" onclick="createInput()">Add a language</button>
+			</div>
+			<?php include("res/php/albums.php"); ?>
+		</div>
+		<?php include("res/php/footer.php"); ?>
 	</body>
 
 	<script>
